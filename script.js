af99695b9855747f745350f741544d026f673029
Madhankumar <108709766+Madhankumar08@users.noreply.github.com>
2022-08-09 12:43:16 +0530

    Add files via upload
    added to github

---
+++ script.js
@@ -0,0 +1,13 @@
+var request=new XMLHttpRequest();
+request.open("get","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+request.send();
+request.onload=function(){
+    var data=JSON.parse(request.response);
+    //console.log(data);
+        for(var i=0;i<data.length;i++){
+        console.log(`Name:${data[i].name},Region:${data[i].region},Sub-region:${data[i].subregion},Population:${data[i].population}`);
+    }
+    
+
+
+}
